956d1d8e0d6b6f527891c19e78a6163726789770
Keiwiscky <keiwiscky@gmail.com>
2022-02-08 11:39:16 +0630

    SwanHtetAung's Project

---
+++ project.js
@@ -0,0 +1,11 @@
+function project(){
+    console.log("Hello world")
+}
+project()
+
+function number(a,b){
+    console.log(a + b)
+}
+number(78,91)
+let list =["Jerry","Tom","aungLa","Metamask","Goal","President"]
+console.log(list[1]);
