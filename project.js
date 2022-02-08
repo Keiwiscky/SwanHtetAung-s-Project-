dbd7a20c6566a270771bf8e1e70f42ab1d6ea16e
Keiwiscky <keiwiscky@gmail.com>
2022-02-08 12:32:53 +0630

    Third Project

--- project.js
+++ project.js
@@ -9,3 +9,7 @@ function number(a,b){
 number(78,91)
 let list =["Jerry","Tom","aungLa","Metamask","Goal","President"]
 console.log(list[1]);
+let num1 = 97;
+let num2 = 4;
+num2 = num1 % num2;
+console.log(num2)
